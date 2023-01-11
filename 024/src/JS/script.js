399e6cbbc68389c3426fca045e8fd96750d2ccf7
ElGeri01 <gerardomagdalena@gmail.com>
2023-01-11 16:16:06 +0100

    a

--- 024/src/JS/script.js
+++ 024/src/JS/script.js
@@ -1,8 +1,39 @@
-const list = document.querySelectorAll('.list');
-function activeLink() {
-    list.forEach((item) =>
-        item.classList.remove('active'));
-    this.classList.add('active');
+
+const uls = document.querySelectorAll("ul");
+
+uls.forEach((ul) => {
+  const resetClass = ul.parentNode.getAttribute("class");
+  const lis = ul.querySelectorAll("li");
+
+  lis.forEach((li) => {
+    li.addEventListener("click", (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      const target = e.currentTarget;
+
+      if (
+        target.classList.contains("active") ||
+        target.classList.contains("follow")
+      ) {
+        return;
+      }
+
+      ul.parentNode.setAttribute(
+        "class",
+        `${resetClass} ${target.getAttribute("data-where")}-style`
+      );
+
+      lis.forEach((item) => clearClass(item, "active"));
+
+      setClass(target, "active");
+    });
+  });
+});
+
+function clearClass(node, className) {
+  node.classList.remove(className);
 }
-list.forEach((item) =>
-    item.addEventListener('click', activeLink));
+
+function setClass(node, className) {
+  node.classList.add(className);
+}
